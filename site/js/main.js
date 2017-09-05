d4fb27bd31426f056bde8ea59ec72a069458c7b5
tolauwae <tom.lauwaerts@ugent.be>
2017-09-05 12:43:20 +0200

    Revert changes on buy method

--- site/js/main.js
+++ site/js/main.js
@@ -4,13 +4,16 @@
 const dappInterface = [ { "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string", "value": "ParkGent" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "balances", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8", "value": "0" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "kill", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "id", "type": "uint256" }, { "name": "regio", "type": "uint256" }, { "name": "tokens", "type": "uint256" } ], "name": "park", "outputs": [ { "name": "succes", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "buyPrice", "outputs": [ { "name": "", "type": "uint256", "value": "26" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string", "value": "P" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "newBuyPrice", "type": "uint256" } ], "name": "setPrices", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" } ], "name": "tickets", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "buy", "outputs": [ { "name": "succes", "type": "bool" } ], "payable": true, "type": "function" }, { "constant": false, "inputs": [ { "name": "to", "type": "address" }, { "name": "amount", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "regio", "type": "uint256" }, { "name": "price", "type": "uint256" } ], "name": "updateRegio", "outputs": [], "payable": false, "type": "function" }, { "inputs": [], "payable": false, "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_from", "type": "address" }, { "indexed": true, "name": "_to", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "Transfer", "type": "event" } ];
 const contractAddress = "0x730b19DCafD1418299fdE830387524bD6F276a14";
 
+// TODO: add Internationalisation with Intl
+// TODO: add account switching?
+
 /**
  * The ParkingRegistry class
  *
  * Should only be created after the window is loaded.
  * @constructor
  */
-function ParkingRegistry() {
+function ParkingRegistry () {
     let self = this;
 
     // Load web3
@@ -20,7 +23,6 @@ function ParkingRegistry() {
     if (typeof web3 !== 'undefined') {
         // Use Mist/MetaMask's provider
         window.web3 = new Web3(web3.currentProvider);
-
     } else {
         console.log('No web3? You should consider trying MetaMask!');
         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
@@ -70,8 +72,8 @@ function ParkingRegistry() {
     //////////
 
     /**
-     * Update the page
-     */
+    * Update the page
+    */
     self.update = function () {
         // update total suply
         contract.totalSupply((error, value) => {
@@ -81,6 +83,7 @@ function ParkingRegistry() {
         });
 
         // update user's balance
+        // TODO: replace address with default account
         contract.balances('0x4219473B52c3D8946057Ed7Ceec851B78d319D74', (error, value) => {
             let field = $('#aantalTokensVanGebruiker');
             field.val(value.valueOf());
@@ -88,7 +91,8 @@ function ParkingRegistry() {
         });
     };
 
-    // TODO: don't write errors to the console
+    // TODO: give alerts some nice styling
+    // TODO: use whisper to confirm the transaction has been mined (if possible)
     self.park = function (id, region, payment) {
         // First execute the method with the call function to check
         // whether or not the park function will resolve correctly
@@ -116,11 +120,11 @@ function ParkingRegistry() {
         contract.setPrices(newPrice, (error, value) => console.log(error, value));
     };
 
-  self.buy = function (amount) {
-      let wei = (amount * 10 ** 16) / buyprice.valueOf();
-    contract.buyPrice((error, buyprice) => {
 
-      contract.buy({value: wei, gas: 2000}, (error, value) => console.log(error, value));
-    });
-  };
+    self.buy = function (amount) {
+        contract.buyPrice((error, buyprice) => {
+            let wei = (amount * 10 ** 16) / buyprice.valueOf();
+            contract.buy({value: wei, gas: 2000}, (error, value) => console.log(error, value));
+        });
+    };
 }
