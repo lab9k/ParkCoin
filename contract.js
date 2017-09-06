6692ef9060a6d5b81aa8fb568fef741436ef97c9
Jefwillems <jefwillems100@gmail.com>
2017-09-06 15:45:58 +0200

    server should now be working. still some problems with static files and libraries

--- contract.js
+++ contract.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(config.infuraNode));
 
 function isAdmin() {
-    let abi = config.abi;
+    let abi = config.contractABI;
     let contractAddress = config.address;
     let instance = new web3.eth.Contract(abi, contractAddress);
 
--- server.js
+++ server.js
@@ -6,12 +6,13 @@ app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+app.use(express.static('site'))
 
 const contract = require('./contract.js');
 
 app.get('/', function (req, res) {
     if (contract.isAdmin()) {
-        //TODO do something
+        res.sendFile("index.html", { root: __dirname + "/site/" })
     } else {
         //TODO do somethng else
     }
