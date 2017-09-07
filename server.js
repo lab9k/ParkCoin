b90c92a880444a5b67832b87e8ba9ef916699715
Jefwillems <jefwillems100@gmail.com>
2017-09-07 15:03:02 +0200

    removed comments

--- server.js
+++ server.js
@@ -11,16 +11,7 @@ app.use(express.static('site'));
 const contract = require('./contract.js');
 
 app.get('/', function (req, res) {
-    //contract.isAdmin().then((value) => {
-    //   if (value === true)
     res.sendFile("index.html", { root: __dirname + "/site/" })
-    //   else
-    //       res.send("not an admin");
-    //  }).catch((error) => {
-    //      res.send(error);
-    //  })
-    //TODO write AJAX function to get admin page content. (address from metamask) => {contract.isAdmin()}
-
 });
 
 app.get('/admin/:address', (req, res) => {
