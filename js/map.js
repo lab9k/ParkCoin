0167fd52d3dcbf29f40cf7effd9c94859f243684
tolauwae <tom.lauwaerts@ugent.be>
2017-09-21 10:57:14 +0200

    fix bug

--- js/map.js
+++ js/map.js
@@ -23,7 +23,56 @@ function initMap() {
 
     georssLayer.setMap(map);
 
-    infoWindow = new google.maps.InfoWindow;
+    let input = document.getElementById('pac-input');
+
+    let autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', map);
+
+    let infowindow = new google.maps.InfoWindow();
+    // let infowindowContent = document.getElementById('infowindow-content');
+    // infowindow.setContent(infowindowContent);
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        // marker.setVisible(false);
+        let place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        placeMarkerAndPanTo(place.geometry.location, map);
+
+        // marker.setPosition(place.geometry.location);
+        // marker.setVisible(true);
+
+        let address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        // infowindowContent.children['place-icon'].src = place.icon;
+        // infowindowContent.children['place-name'].textContent = place.name;
+        // infowindowContent.children['place-address'].textContent = address;
+        // infowindow.open(map, marker);
+    });
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
